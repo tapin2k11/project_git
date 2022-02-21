4191b50ee2bb5ec35e42e5e6fef7bb687c03b96f
tapin2k11 <99002701+tapin2k11@users.noreply.github.com>
2022-02-21 17:59:20 -0500

    Update VS_Py.py
    New Commit

--- VS_Py.py
+++ VS_Py.py
@@ -1,1 +1,7 @@
 
+
+for i in range(1,51):
+     print("Fizz" * (i%3 == 0) + "Buzz" * (i%5 == 0) or str(i)) 
+
+
+print(list(map(lambda  i: "Fizz" * (i%3 == 0) + "Buzz" * (i%5 == 0) or str(i), range(1,101))))
