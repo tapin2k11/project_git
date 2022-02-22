d8887cc4e49e68e839e3cc8557ddb2310f18a312
tapin2k11 <99002701+tapin2k11@users.noreply.github.com>
2022-02-21 20:21:28 -0500

    another commit

--- VS_Py.py
+++ VS_Py.py
@@ -3,5 +3,11 @@
 for i in range(1,51):
      print("Fizz" * (i%3 == 0) + "Buzz" * (i%5 == 0) or str(i)) 
 
-
 print(list(map(lambda  i: "Fizz" * (i%3 == 0) + "Buzz" * (i%5 == 0) or str(i), range(1,101))))
+
+
+USE AdventureWorks2019;
+
+GO
+
+select * from HumanResources.Department;
